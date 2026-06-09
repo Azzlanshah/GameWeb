c4f85beb8552c10767d171cad78dc6b0586726d9
Azzlanshah <azzlanshah99@gmail.com>
2026-06-09 23:06:48 +0700

    Update: menambahkan gravity, lompat, dan kontrol keyboard pada player

--- src/js/classes/Player.js
+++ src/js/classes/Player.js
@@ -8,10 +8,41 @@ class Player {
             x: props.position.x,
             y: props.position.y
         }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.gravity = 0.5
+        this.heightJump = 100
+    }
+
+    movement(direction) {
+        console.log(direction)
+        switch(direction) {
+            case "ArrowUp":
+                this.velocity.y = -Math.sqrt(2 * this.gravity * this.heightJump)
+                break
+            case "ArrowLeft":
+                this.velocity.x = -1
+                break
+            case "ArrowRight":
+                this.velocity.x = +1
+                break
+            default:
+                break
+        }
     }
 
     update() {
-        this.position.x += 1
+        const ground = canvas.height - this.height
+
+        this.velocity.y += this.gravity
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if(player.position.y > ground) {
+            this.position.y = ground
+        }
     }
 
     create() {
